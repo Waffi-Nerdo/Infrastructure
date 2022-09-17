a219762244924f47996f0f7fc2b4483a882be557
waffi-nerdo <waffi-nerdo@gmail.com>
2022-09-17 17:29:23 -0400

    remove variables

--- Terraform/upload-statefile.tf
+++ Terraform/upload-statefile.tf
@@ -1,9 +1,9 @@
 #Upload State file to S3 bucket
 terraform {
   backend "s3" {
-    bucket = var.bucket
-    key    = var.key
-    region = var.region
-    assume_role_policy_arns = var.bucketarns
+    bucket = "my-infrastructure-ifeoluwa"
+    key    = "statefile/${var.env}.tfstate"
+    region = "us-east-1"
+    #assume_role_policy_arns = []
   }
 }
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -4,21 +4,6 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "bucket" {
-    description = "The S3 bucket where the State File will be uploaded"
-    type = string
-}
-
-variable "key" {
-    description = "The path where the statefile will be stored"
-    type = string
-}
-
-variable "bucketarns" {
-  description = "Permissions needed to upload state file to S3 bucket"
-  type = set(string)
-}
-
 variable "cidr_block" {
   description = "VPC Cidr block"
   type = string
