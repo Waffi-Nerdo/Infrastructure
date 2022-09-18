a7144f4402018ff3eaa2539c933f58fc3f88a604
waffi-nerdo <waffi-nerdo@gmail.com>
2022-09-18 11:46:16 -0400

    update terraform files

--- Terraform/backend-configuration.tf
+++ Terraform/backend-configuration.tf
@@ -1,9 +1,10 @@
 #Upload State file to S3 bucket
 terraform {
   backend "s3" {
-    bucket = "my-infrastructure-ifeoluwa"
-    key    = "statefile/dev.tfstate"
+    bucket = "myinfrastructure2022udeagha"
+    key    = "dev.tfstate"
     region = "us-east-1"
+    dynamodb_table = "myinfrastructure"
     #assume_role_policy_arns = []
   }
 }
