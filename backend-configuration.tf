a8caa19741c560f9b673ee5f72c869f4dbd14a0b
waffi-nerdo <waffi-nerdo@gmail.com>
2022-09-19 11:03:07 -0400

    move config files to root directory

---
+++ backend-configuration.tf
@@ -0,0 +1,10 @@
+#Upload State file to S3 bucket
+terraform {
+  backend "s3" {
+    bucket = "myinfrastructure2022udeagha"
+    key    = "dev.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "myinfrastructure"
+    #assume_role_policy_arns = []
+  }
+}
