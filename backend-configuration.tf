c0bf8b6d5401008dffe130bb888cf5e1f85182e3
waffi-nerdo <waffi-nerdo@gmail.com>
2022-09-19 11:04:13 -0400

    fmt

--- backend-configuration.tf
+++ backend-configuration.tf
@@ -1,9 +1,9 @@
 #Upload State file to S3 bucket
 terraform {
   backend "s3" {
-    bucket = "myinfrastructure2022udeagha"
-    key    = "dev.tfstate"
-    region = "us-east-1"
+    bucket         = "myinfrastructure2022udeagha"
+    key            = "dev.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "myinfrastructure"
     #assume_role_policy_arns = []
   }
